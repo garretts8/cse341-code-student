2071b8f2a50b3e38456ea3b102c61bd05c248a9c
garretts8 <gar21085@byui.edu>
2026-02-25 19:39:54 -0700

    Still working on swagger. Ensuring enpoint authorization

--- audiobook_library/backend/routes/swagger.js
+++ audiobook_library/backend/routes/swagger.js
@@ -3,6 +3,24 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+
+router.get('/', (req, res, next) => {
+  // Clone the swagger document
+  const swaggerCopy = JSON.parse(JSON.stringify(swaggerDocument));
+
+  // Dynamically set the host based on request
+  swaggerCopy.host = req.get('host');
+
+  // To show the Authorize button
+  const swaggerOptions = {
+    explorer: true,
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+
+  // Pass modified document to swaggerUi.setup
+  swaggerUi.setup(swaggerCopy, swaggerOptions)(req, res, next);
+});
 
 module.exports = router;
